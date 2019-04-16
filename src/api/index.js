c615c316ae6f8b00aef49f3718aa779f5c633fdb
wuxuan <wuxuan@yijiupi.com>
2019-04-16 17:00:15 +0800

    联调

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 axios.interceptors.request.use(function (config) {
 
 	if (config.data) {
-		config.data.userId = "6348352047144357000";  //用户id 
+		//config.data.userId = "6348352047144357000";  //用户id 
 		//config.data.dealerId = "19990530";  //店铺id
 	}
 	// const token = storage.get('TOKEN', '');
