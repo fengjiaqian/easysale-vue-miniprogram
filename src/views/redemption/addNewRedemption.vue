d6a8c42b0092575b8f22ac096c67fa964858c614
zhujie <>
2019-04-25 17:15:15 +0800

    投诉、兑奖模块静态页面

--- src/views/redemption/addNewRedemption.vue
+++ src/views/redemption/addNewRedemption.vue
@@ -1,38 +1,69 @@
 <template>
     <div id="addNewRedemption">
         <div class="goods-box">
-            <p class="title">兑奖商品</p>
+            <p class="title" :style="{borderWidth:redemptionGoods.length?'0.5px':'0.25px'}">兑奖商品</p>
+            <!--<div class="dividing-line-2" v-if="redemptionGoods.length"></div>-->
             <div class="good-warp">
-                <p v-if="!redemptionGoods.length" class="add-tip" @click="toAddRedemptionGoods()">+添加兑奖商品</p>
+                <div v-if="redemptionGoods.length">
+                    <ul>
+                        <li v-for="(item,index) in redemptionGoods">
+                            <div class="goods-list-box">
+                                <div class="goods-info">
+                                    <div class="img-box">
+                                        <img>
+                                    </div>
+                                    <p class="goods-name">洋河蓝色经典 梦之蓝M6 52度梦之蓝M6 52度梦之蓝M6 52度</p>
+                                    <div class="del-btn">删除</div>
+                                </div>
+                                <div class="count-box">
+                                    <span class="font-30-333">兑奖数量：</span>
+                                    <number-picker :product="item"></number-picker>
+                                </div>
+                                <p class="dividing-line" v-show="index<(redemptionGoods.length-1)"></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <p class="add-tip" :style="{marginTop:redemptionGoods.length?'12px':'0',borderWidth:redemptionGoods.length?'0.5px':'0.01px'}" @click="toAddRedemptionGoods()">+添加兑奖商品</p>
             </div>
         </div>
         <div class="remark-box">
-            <p class="title">备注</p>
+            <p class="title ">备注</p>
             <textarea class="remark-input" id="remark" cols="30" rows="6" placeholder="请输入内容"
                       v-model="remark"></textarea>
         </div>
         <button class="submit-btn">提交</button>
     </div>
-    <!--<someComponent></someComponent>-->
 </template>
 
 <script>
-    //import someComponent from './someComponent'
+    import numberPicker from "components/number-picker.vue";
+
     export default {
         name: 'addNewRedemption',
         data() {
             return {
-                redemptionGoods: [],
-                remark:''
+                redemptionGoods: [
+                    {
+                        buyCount: 1,
+                    },
+                    {
+                        buyCount: 2,
+                    },
+                    {
+                        buyCount: 2,
+                    }
+                ],
+                remark: ''
             }
         },
-        component: {
-            //someComponent
+        components: {
+            numberPicker
         },
 
-        methods:{
+        methods: {
             //跳转到添加兑奖商品
-            toAddRedemptionGoods(){
+            toAddRedemptionGoods() {
                 this.$router.push({
                     name: "addRedemptionGoods",
                 });
@@ -53,17 +84,18 @@
 
         }
         .add-tip {
-            mt(32);
+            pt(32);
             c(#FF5638);
             ft(28);
             text-align center
+            border-top 1px solid #EDEDED
 
         }
         .title {
             padding-bottom 24px;
             c(#333);
             ft(30);
-            fb()
+            fb();
             border-bottom 1px solid #EDEDED
         }
         .remark-input {
@@ -84,7 +116,8 @@
             border: 0;
             outline: none;
         }
-        textarea::-webkit-input-placeholder {
+        textarea:
+        :-webkit-input-placeholder {
             color: #BDBDBD !important;
             font-size: 30px;
             text-align: left;
@@ -98,6 +131,65 @@
             outline: none;
             font-size: 30px;
             c(#333);
+        }
+        .goods-list-box {
+            display flex;
+            flex-direction column;
+            .goods-info {
+                position relative;
+                display flex;
+                flex-direction row;
+                padding 24px 0
+                .img-box {
+                    w(120)
+                    h(120)
+                    mr(24)
+                    border-radius: 6px
+                }
+                 img {
+                    w(120)
+                    h(120)
+
+                }
+                .goods-name {
+                    c(#333);
+                    ft(30);
+                    text-overflow-2();
+                    mr(149)
+                }
+                .del-btn {
+                    position absolute;
+                    right 0;
+                    w(80);
+                    h(40);
+                    lh(40)
+                    border-radius: 16px;
+                    border: 2px solid rgba(221, 221, 221, 1);
+                    c(#999);
+                    font-size 22px;
+                    text-align center
+                }
+            }
+            .count-box{
+                display flex;
+                flex-direction row;
+                align-items center;
+                justify-content flex-end
+                .font-30-333{
+                    ft(30);
+                    c(#333)
+                }
+            }
+            .dividing-line{
+                h(2);
+                bg(#EDEDED);
+                ml(168)
+                mt(24)
+            }
+            .dividing-line-2{
+                h(2);
+                bg(#EDEDED);
+            }
         }
 
     }
--- src/views/redemption/addRedemptionGoods.vue
+++ src/views/redemption/addRedemptionGoods.vue
@@ -1,38 +1,98 @@
 <template>
     <div id="addRedemptionGoods">
-        <search-bar @emitEvt="_searchKeyChange" :emit="true"></search-bar>
+        <search-bar :isFixed="true" :isSearch="true" placeholder="请输入商品名称" :searchKey="searchKey"
+                    @emitEvt="_searchKeyChange"></search-bar>
+        <scroll class="product-list-scroll"
+                :data="productList"
+                :probeType="3"
+                :pullup="true"
+                @scrollToEnd="loadMoreProducts"
+                ref="productScrollDom"
+        >
+            <ul>
+                <li v-for="(item, index) in productList">
+                    <div class="goods-box">
+                        <img v-lazy="selectImg[0]" class="select-img">
+                        <img class="goods-img">
+                        <p class="goods-name">洋河蓝色经典 梦之蓝M6 52度</p>
+                    </div>
+                </li>
+            </ul>
+        </scroll>
         <button class="certain-btn">确定</button>
     </div>
     <!--<someComponent></someComponent>-->
 </template>
 
 <script>
-    import searchBar from "components/searchBar.vue";
+    import searchBar from "components/header.vue";
+    import scroll from "components/scroll.vue";
+    import ic1 from "../../assets/images/icon-check.png";
+    import ic2 from "../../assets/images/icon-checked.png";
+
+    const selectImg = [ic1, ic2,];
     export default {
         name: '',
         data() {
             return {
-                msg: "Hello Vue.js"
+                searchKey: '',
+                productList: [1, 2, 3],
+                selectImg: selectImg
             }
         },
         components: {
-            searchBar
+            searchBar,
+            scroll
         },
-        methods:{
+        methods: {
             _searchKeyChange(searchKey) {
-
+                this.searchKey = searchKey
             },
+
+            loadMoreProducts() {
+
+            }
 
         }
     }
 </script>
 
 <style lang="stylus">
-    #addRedemptionGoods{
+    #addRedemptionGoods {
         width 100%;
+        height 100%
         overflow scroll;
         bg(#fff);
-        .certain-btn{
+        .product-list-scroll {
+            mt(98);
+            mb(98)
+        }
+        .goods-box {
+            display flex
+            flex-direction row;
+            margin:24px;
+            .goods-img{
+                w(120);
+                h(120);
+                bg(#f6f6f6)
+                border-radius:6px;
+                margin 0 24px;
+
+            }
+            .goods-name{
+                c(#333);
+                ft(30);
+
+            }
+
+
+        }
+        .select-img {
+            w(40)
+            h(40)
+            mt(40)
+        }
+        .certain-btn {
             h(98)
             position: fixed;
             width: 100%;
@@ -46,7 +106,6 @@
             border: 0;
             outline: none;
         }
-
 
     }
 
