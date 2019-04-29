d9b4fca6ebd1bb21f404f5e0484852c0bbe25bc1
zhujie <>
2019-04-29 14:13:27 +0800

    UI

--- src/views/redemption/list-item.vue
+++ src/views/redemption/list-item.vue
@@ -27,7 +27,7 @@
             <div class="tips">{{listData.customerAward.replyContent}}</div>
         </div>
         <p class="state">{{stateList[0]}}</p>
-        <div class="btn-warp" :style="{justifyContent:isNull? 'flex-end':'space-between'}">
+        <div class="btn-warp" :style="{justifyContent:isNull ||isCustomer? 'flex-end':'space-between'}">
             <p v-if="listData.saleMan.dealingName&&tabState==1&&!isCustomer" class="saleMan">
                 销售人员-{{listData.saleMan.dealingName}}已处理</p>
             <p v-if="listData.saleMan.dealingName&&tabState==0&&!isCustomer" class="saleMan">
