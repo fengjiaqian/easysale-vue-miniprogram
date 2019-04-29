31992e50b5d997aa7f17965c120af42f4f2b8c72
zhujie <>
2019-04-29 18:20:34 +0800

    申请经销商审核页面调整

--- src/views/mine/mine-user/applyDealer.vue
+++ src/views/mine/mine-user/applyDealer.vue
@@ -1,151 +1,124 @@
-
 <template>
-  <div class="common pt90">
-    <m-header :isFixed="true"></m-header>
-    <div class="status">
-      <div class="mode">
-        <div class="way">申请状态：:</div>
-        <div class="fettle">审核中</div>
-      </div>
-      <!-- <div class="describe">经销商正在处理您的问题，请耐心等待！</div> -->
-    </div>
-    <div class="license" v-if="applyDealerInfo.logoIamgeUrls.length">
-      <div class="trade">营业执照</div>
-      <div class="slider-wrapper bg-w">
-        <div class="slider-body">
-          <slider ref="slider_dom" :loop="applyDealerInfo.logoIamgeUrls.length>1?true:false">
-            <div class="banner-item" v-for="item in applyDealerInfo.logoIamgeUrls">
-              <a href="javascript:;">
-                <img :src="item">
-              </a>
-            </div>
-          </slider>
+    <div class="body">
+        <m-header :isFixed="true"></m-header>
+        <div class="title-box">
+            <p>申请状态：<span class="red-font">审核中</span></p>
         </div>
-      </div>
+        <!-- <div class="describe">经销商正在处理您的问题，请耐心等待！</div> -->
+        <div class="license-box" v-if="logoImageUrls.length">
+            <p class="border-line">营业执照</p>
+            <div class="slider-body">
+                <slider ref="slider_dom" :loop="logoImageUrls.length>1?true:false">
+                    <div class="banner-item" v-for="item in logoImageUrls">
+                        <img v-lazy="item" class="img">
+                    </div>
+                </slider>
+            </div>
+        </div>
+        <div class="license-box" v-if="applyDealerInfo&&applyDealerInfo.name">
+            <p class="border-line">个人信息</p>
+            <p class="font-30-666 margin-top">姓名：{{applyDealerInfo.name}}</p>
+            <p class="font-30-666 line">联系电话：{{applyDealerInfo.phone}}</p>
+            <p class="font-30-666 margin-top">{{applyDealerInfo.shopName}}</p>
+            <p class="font-30-666 text-warp">{{applyDealerInfo.address}}</p>
+        </div>
     </div>
-    <div class="infor" v-if="applyDealerInfo.name">
-      <div class="trade">个人信息</div>
-      <div class="news">
-        <div class="name">姓名 : {{applyDealerInfo.name}}</div>
-        <div>联系电话 : {{applyDealerInfo.phone}}</div>
-      </div>
-      <div class="detail">
-        <div class="name">{{applyDealerInfo.shopName}}</div>
-        <div>{{applyDealerInfo.address}}</div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-import icBanner from "@/assets/images/ic-banner.png";
-import { findApplyDealerInfo } from "api/fetch/endCustomer";
-import slider from "components/slider.vue";
-export default {
-  data() {
-    return {
-      applyDealerInfo: { logoIamgeUrls: [] },
-      icBanner: icBanner
+    import icBanner from "@/assets/images/ic-banner.png";
+    import {findApplyDealerInfo} from "api/fetch/endCustomer";
+    import slider from "components/slider.vue";
+
+    export default {
+        data() {
+            return {
+                applyDealerInfo: {},
+                logoImageUrls: [],
+                icBanner: icBanner
+            };
+        },
+        components: {
+            slider
+        },
+        created() {
+            this._findApplyDealerInfo();
+        },
+        methods: {
+            _findApplyDealerInfo() {
+                findApplyDealerInfo()
+                    .then(res => {
+                        if (res.data) {
+                            this.applyDealerInfo = res.data;
+                            this.logoImageUrls = [...res.data.logoIamgeUrls]
+                        }
+                    }).catch(err => {});
+            }
+        }
     };
-  },
-  components: {
-    slider
-  },
-  computed: {},
-  created() {
-    this._findApplyDealerInfo();
-  },
-  methods: {
-    _findApplyDealerInfo() {
-      findApplyDealerInfo()
-        .then(res => {
-          this.applyDealerInfo = res.data;
-        })
-        .catch(err => {});
-    }
-  }
-};
 </script>
 
 <style lang="stylus" scoped>
-.slider-body {
-  pos(relative);
-  overflow: hidden;
-}
+    .body {
+        mt(90);
+        width 100%;
+        height 100%;
+        bg(#f6f6f6)
+    }
 
-.common {
-  background-color: #F6F6F6;
-  position: relative;
-  height: 100%;
-  overflow: hidden;
-}
+    .title-box {
+        bg(#fff);
+        ft(34);
+        c(#333);
+        fb();
+        padding 16px 24px;
 
-.common .status {
-  padding: 40px 24px 15px 24px;
-  background: white;
-}
+    }
 
-.common .status .mode {
-  width: 100%;
-  height: 46px;
-  overflow: hidden;
-  margin-bottom: 10px;
-}
+    .red-font {
+        c(#FF5638)
+    }
 
-.common .status .mode .way {
-  float: left;
-  font-size: 34px;
-  font-weight: bold;
-  color: rgba(51, 51, 51, 1);
-  line-height: 46px;
-  margin-right: 20px;
-  display: inline;
-}
+    .license-box {
+        ft(30);
+        c(#333);
+        margin 24px;
+        padding 16px 24px;
+        bg(#fff)
+    }
 
-.common .status .mode .fettle {
-  float: left;
-  font-size: 34px;
-  font-weight: bold;
-  color: #FF5638;
-  line-height: 46px;
-  margin-right: 20px;
-  display: inline;
-}
+    .border-line {
+        fb()
+        pb(24)
+        border-bottom 1px solid #f6f6f6
+    }
 
-.common .status .describe {
-  width: 468px;
-  height: 36px;
-  font-size: 26px;
-  color: rgba(153, 153, 153, 1);
-  line-height: 36px;
-}
+    .img {
+        squ(300)
+    }
 
-.common .license, .common .infor {
-  width: 96%;
-  margin: 2%;
-  background: #fff;
-}
+    .slider-body {
+        mt(58)
+        pos(relative);
+        overflow: hidden;
+    }
 
-.common .license .trade, .common .infor .trade {
-  padding: 24px;
-  font-size: 32px;
-  font-weight: bold;
-  color: rgba(51, 51, 51, 1);
-  line-height: 42px;
-  border-bottom: 1PX solid #f6f6f6;
-}
+    .margin-top {
+        mt(16)
+    }
 
-.common .infor .news, .common .infor .detail {
-  padding: 24px;
-  font-size: 30px;
-  font-family: PingFang-SC-Regular;
-  font-weight: 400;
-  color: rgba(102, 102, 102, 1);
-  line-height: 42px;
-  border-bottom: 1PX solid #f6f6f6;
-}
+    .font-30-666 {
+        c(#666);
+        ft(30);
+        padding 8px
+    }
 
-.common .infor .news .name, .common .infor .detail .name {
-  margin-bottom: 10px;
-}
+    .line {
+        pb(16)
+        border-bottom 1px solid #f6f6f6
+    }
+
+    .text-warp {
+        text-overflow-2()
+    }
 </style>
