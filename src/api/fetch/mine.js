a2008da6c4eaad8129f36ed77469adb17a32815d
zhujie <>
2019-05-08 19:29:00 +0800

    loading

--- src/api/fetch/mine.js
+++ src/api/fetch/mine.js
@@ -358,7 +358,8 @@ function synthesisroutineimg(params) {
 	return axios({
 		method: 'post',
 		url: url,
-		data: params
+		data: params,
+		loading:true
 	}).then((res) => {
 		return Promise.resolve(res.data)
 	}).catch(res => {
