b321a252f0094de450de79d085ca828a0eb31af7
panxu <12345678>
2019-05-28 13:38:23 +0800

    适配ipx

--- src/App.vue
+++ src/App.vue
@@ -8,27 +8,43 @@
 </template>
 
 <script>
-import FastClick from "fastclick";
-export default {
-  name: "app",
-  data() {
-    return {
-      show: false
-    };
-  },
-  created() {},
-  mounted() {
-   // FastClick.attach(document.body);
-    document.body.addEventListener("touchend", function(el) {
-      if (el.target.tagName != "INPUT") {
-        const els = document.querySelectorAll("input");
-        Array.prototype.slice.call(els).forEach(input => {
-          input.blur();
-        });
+  import FastClick from "fastclick";
+  import Vue from 'vue'
+  // Vue.prototype.isIphoneX = true;
+  export default {
+    name: "app",
+    data() {
+      return {
+        show: false
+      };
+    },
+    created() {
+      this.checkIpx();
+    },
+    mounted() {
+      // FastClick.attach(document.body);
+      document.body.addEventListener("touchend", function(el) {
+        if (el.target.tagName != "INPUT") {
+          const els = document.querySelectorAll("input");
+          Array.prototype.slice.call(els).forEach(input => {
+            input.blur();
+          });
+        }
+      });
+    },
+    methods:{
+      checkIpx(){
+        var u = navigator.userAgent;
+        var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+        if (isIOS) {
+          if (screen.height == 812 && screen.width == 375) {
+            //是iphoneX
+            Vue.prototype.isIphoneX = true;
+          }
+        }
       }
-    });
-  }
-};
+    }
+  };
 </script>
 
 <style lang="stylus"></style>
--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -3,7 +3,7 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
-    <div class="tab-container">
+    <div class="tab-container" :class="{'isIphoneX':isIphoneX}" >
       <router-link tag="div" class="tab-item" to="/navi/home">
         <a class="tab-home" href="javascript:;"></a>
         <span>首页</span>
@@ -21,91 +21,110 @@
 </template>
 
 <script>
-import storage from "common/storage";
-export default {
-  name: "tab",
-  data() {
-    return {};
-  },
-  created() {},
-  mounted() {},
-  methods: {}
-};
+  import storage from "common/storage";
+  import Vue from 'vue'
+  export default {
+    name: "tab",
+    data() {
+      return {
+
+        isIphoneX:this.isIphoneX,
+      };
+    },
+    created() {
+      // this.checkIpx()
+    },
+    mounted() {
+    },
+    methods: {
+      // checkIpx(){
+      //   var u = navigator.userAgent;
+      //   var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+      //   if (isIOS) {
+      //     if (screen.height == 812 && screen.width == 375) {
+      //       //是iphoneX
+      //       Vue.prototype.isIphoneX = true;
+      //     }
+      //   }
+      // }
+    }
+  }
 </script>
 
 <style lang='stylus'>
-
-.ios-scroll-smooth {
-  width: 100%;
-  height: 100%;
-  -webkit-overflow-scrolling: touch;
-}
-
-.tab-container {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  z-index: 900;
-  width: 100%;
-  height: 98px;
-  background-color: #FFFFFF;
-  display: flex;
-  border-top: 1PX solid rgba(0, 0, 0, 0.1);
-}
-@supports (bottom: env(safe-area-inset-bottom)) {
-  .tab-container{
-    padding-bottom: env(safe-area-inset-bottom);
-  }
-}
-
-.tab-item {
-  flex-1();
-  padding: 3px 0;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: space-around;
-
-  >a {
-    block();
-    squ(50);
-    background-size: contain;
+  .ios-scroll-smooth {
+    width: 100%;
+    height: 100%;
+    -webkit-overflow-scrolling: touch;
   }
 
-  >span {
-    line-height: 1.2;
-    ft(22);
-    c(#999);
+  .tab-container {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 900;
+    width: 100%;
+    /*height: 98px;*/
+    background-color: #FFFFFF;
+    display: flex;
+    border-top: 1PX solid rgba(0, 0, 0, 0.1);
   }
 
-  .tab-home {
-    background-image: url('./../assets/images/icon-shouye.png');
+  .tab-container.isIphoneX{
+    padding-bottom 34px;
   }
 
-  .tab-mine {
-    background-image: url('./../assets/images/icon-mine.png');
-  }
 
-  .tab-order {
-    background-image: url('./../assets/images/icon-orders.png');
-  }
+  .tab-item {
+    flex-1();
+    padding: 3px 0;
+    height: 98px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-around;
 
-  &.router-link-active {
-    span {
-      c(#FF5638);
+    >a {
+      block();
+      squ(50);
+      background-size: contain;
+    }
+
+    >span {
+      line-height: 1.2;
+      block();
+      ft(22);
+      c(#999);
     }
 
     .tab-home {
-      background-image: url('./../assets/images/icon-shouye-active.png');
+      background-image: url('./../assets/images/icon-shouye.png');
     }
 
     .tab-mine {
-      background-image: url('./../assets/images/icon-mine-active.png');
+      background-image: url('./../assets/images/icon-mine.png');
     }
 
     .tab-order {
-      background-image: url('./../assets/images/icon-orders-active.png');
+      background-image: url('./../assets/images/icon-orders.png');
+    }
+
+    &.router-link-active {
+      span {
+        c(#FF5638);
+      }
+
+      .tab-home {
+        background-image: url('./../assets/images/icon-shouye-active.png');
+      }
+
+      .tab-mine {
+        background-image: url('./../assets/images/icon-mine-active.png');
+      }
+
+      .tab-order {
+        background-image: url('./../assets/images/icon-orders-active.png');
+      }
     }
   }
-}
 </style>
