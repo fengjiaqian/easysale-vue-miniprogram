9eab058e8419e288e24830ef70d2fa7a8574dac9
wangbo <wb549791>
2019-08-05 16:34:06 +0800

    調整

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,12 @@ module.exports = {
                 "styleLibraryName": "theme-chalk"
             }
         ],
-
+    ],
+    rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',//注意elementUI的源码使用ES6需要解析
+            include: [resolve('src'), resolve('test'),resolve('/node_modules/element-ui/src'),resolve('/node_modules/element-ui/packages')]
+        }
     ]
 }
