adbb32d4e2ddc5dd7a4c34538ba2166b1f07f5c2
wangbo <wb549791>
2019-08-05 16:38:44 +0800

    調整

--- babel.config.js
+++ babel.config.js
@@ -11,12 +11,5 @@ module.exports = {
                 "styleLibraryName": "theme-chalk"
             }
         ],
-    ],
-    rules: [
-        {
-            test: /\.js$/,
-            loader: 'babel-loader',//注意elementUI的源码使用ES6需要解析
-            include: [resolve('src'), resolve('test'),resolve('/node_modules/element-ui/src'),resolve('/node_modules/element-ui/packages')]
-        }
     ]
 }
--- vue.config.js
+++ vue.config.js
@@ -127,4 +127,11 @@ module.exports = {
 
         }
     },
+    rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',//注意elementUI的源码使用ES6需要解析
+            include: [resolve('src'), resolve('test'),resolve('/node_modules/element-ui/src'),resolve('/node_modules/element-ui/packages')]
+        }
+        ]
 };
